343adbd5845f3c16863bbb57fa5baca33b534aea
shakiel17 <hisoka.aboy@gmail.com>
2025-06-13 13:04:00 +0800

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -57,7 +57,7 @@ $route['save_deduction'] = 'pages/save_deduction';
 $route['manage_deduction/(:any)/(:any)'] = 'pages/manage_deduction/$1/$2';
 //========================Payroll Module=======================================
 $route['print_employee_list'] = 'pages/print_employee_list';
-$route['delete_adjustment/(:any)/(:any)/(:any)'] = 'pages/delete_adjustment/$1/$2/$';
+$route['delete_adjustment/(:any)/(:any)/(:any)/(:any)'] = 'pages/delete_adjustment/$1/$2/$3/$4';
 $route['save_adjustment'] = 'pages/save_adjustment';
 $route['manage_adjustment/(:any)/(:any)'] = 'pages/manage_adjustment/$1/$2';
 $route['print_payslip/(:any)'] = 'pages/print_payslip/$1';
